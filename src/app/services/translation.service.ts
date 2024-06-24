d74cd5f00bd50b813322b54133d3ca73384745f5
Paul Pietzko <paul.pietzko@icloud.com>
2024-06-24 10:06:41 +0200

    Changed language service to be saved in a cookie insted of the localstorage

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -14,7 +14,7 @@ export class TranslationService {
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
     if (isPlatformBrowser(this.platformId)) {
-      const savedLang = localStorage.getItem('lng');
+      const savedLang = this.getCookie('lng');
       if (savedLang) {
         this.defaultLang = savedLang;
       }
@@ -22,11 +22,32 @@ export class TranslationService {
       this.translateService.use(this.defaultLang);
     }
   }
-
+  
   changeLang(lang: string) {
     this.translateService.use(lang);
     if (isPlatformBrowser(this.platformId)) {
-      localStorage.setItem('lng', lang);
+      this.setCookie('lng', lang, 365);
     }
+  }
+  
+  private setCookie(name: string, value: string, days: number) {
+    let expires = "";
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  }
+  
+  private getCookie(name: string) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+      let c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
   }
 }
