f6560fcf766a220b1f3d7621d8e8e948840a0cef
Paul Pietzko <paul.pietzko@icloud.com>
2024-06-24 14:11:11 +0200

    Added languages: fr, it, ja, zh

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,1 +1,8 @@
-export const languages = ['en', 'de'];
+export const languages = [ // Language identifiers after ISO 639-1 
+    'en', // English
+    'de', // German
+    'fr', // French
+    'it', // Italian
+    'ja', // Japanese
+    'zh' // Chinese
+];
